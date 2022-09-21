734f7a563b87b7b9c120c6e1ffa3ef348f2b604e
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2022-09-21 17:39:02 +0200

    Solucionado strtrim

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 11:18:57 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/09/21 10:58:15 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:25:14 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ char	*ft_strchr(const char *s, int c)
 	while (*p && *p != (char)c)
 		p++;
 	if (*p != (char)c)
-		return (0);
+		return (NULL);
 	return (p);
 }
--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,52 +6,36 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 16:02:54 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/09/19 16:26:22 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:32:14 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-int	ft_isincluded(int c, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*buf;
-	char	*start;
-	char	*end;
+	size_t	start;
 	size_t	len;
 	size_t	ss_len;
 
+	start = 0;
 	len = ft_strlen(s1);
-	start = (char *)s1;
-	end = (char *)s1 + len;
-	while (*start && ft_isincluded(*start, set))
-		start++;
-	while (end >= s1 && ft_isincluded(*end, set))
-		end--;
-	ss_len = end - start;
-	if (ss_len < 0)
-		ss_len = 0;
+	if (len > 0)
+	{
+		while (start < len && ft_strchr(set, s1[start]))
+			start++;
+		while (len > start && ft_strchr(set, s1[len - 1]))
+			len--;
+	}
+	ss_len = len - start;
 	buf = malloc(ss_len + 1);
 	if (buf)
 	{
 		if (ss_len > 0)
-			ft_memcpy(buf, start, ss_len);
+			ft_memcpy(buf, s1 + start, ss_len);
 		buf[ss_len] = 0;
-		return (buf);
 	}
-	return (0);
+	return (buf);
 }
